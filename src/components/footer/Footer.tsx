03344d60f53e25755bc2da3a70c0494110092a85
chandan <chandan868@gmail.com>
2025-03-22 03:22:50 +0530

    finished till piechart 1hr17min

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <span>chan-admin</span>
+            <span>Chan Admin Dashboard</span>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/pages/users/Users.tsx
@@ -0,0 +1,8 @@
+import './users.scss'
+const Users = () => {
+    return (
+        <div className="users">Users story</div>
+    )
+}
+
+export default Users;
